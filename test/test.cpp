2f88074a237fb3a3317435eff775f173a890eea0
ZinovievVladimir <vladimirzinoviev97@gmail.com>
2016-10-21 08:04:39 +0300

    first commit

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "add.h"
+
+TEST(Addition, CanAddTwoNumbers)
+{
+  EXPECT_TRUE(add(2, 2) == 4);
+}
+
+int main(int ac, char* av[])
+{
+  testing::InitGoogleTest(&ac, av);
+  return RUN_ALL_TESTS();
+}
